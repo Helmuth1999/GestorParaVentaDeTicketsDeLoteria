df86da63a02288bbac028c6a655e571123779e0f
SergioJimenezM <anubarakdewaarimasen@gmail.com>
2019-06-06 15:00:00 -0600

    Añadida la clase SorteoDetalleVentas al modelo

---
+++ GestorParaVentaDeLoteria/GestorParaVentaDeLoteria.Modelo/SorteoDetalleVentas.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations;
+
+namespace GestorParaVentaDeLoteria.Modelo
+{
+    public class SorteoDetalleVentas
+    {
+
+        public int Id { get; set; }
+        [Display(Name = "Identificador de Sorteos")]
+        public int IdSorteos { get; set; }
+
+        public int Numero { get; set; }
+
+        public decimal Monto { get; set; }
+
+    }
+}
