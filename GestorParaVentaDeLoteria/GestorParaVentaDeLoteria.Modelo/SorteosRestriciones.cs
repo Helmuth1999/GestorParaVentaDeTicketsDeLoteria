f7b5839b345482c92d7fa96315f27dd40e7771f8
Pedro Pablo Lumbí Álvarez <ppablola-98@hotmail.com>
2019-06-06 23:07:22 -0600

    sincronizando cambios y  añadir SorteosRestriciones

--- GestorParaVentaDeLoteria/GestorParaVentaDeLoteria.Modelo/SorteosRestriciones.cs
+++ GestorParaVentaDeLoteria/GestorParaVentaDeLoteria.Modelo/SorteosRestriciones.cs
@@ -1,20 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
 namespace GestorParaVentaDeLoteria.Modelo
 {
-    using System;
-    using System.Collections.Generic;
-    using System.ComponentModel.DataAnnotations;
-    using System.ComponentModel.DataAnnotations.Schema;
-    using System.Data.Entity.Spatial;
-
     public class SorteosRestriciones
     {
         public int Id { get; set; }
-
         public int Numero { get; set; }
-
         public decimal MontoMáximo { get; set; }
-
         public int IdSorteos { get; set; }
-
     }
 }
