4d774f5aa3f288e15c4bec291f8a0c8d0f53d445
SergioJimenezM <anubarakdewaarimasen@gmail.com>
2019-06-06 22:07:08 -0600

    Añadido sorteos y estado

---
+++ GestorParaVentaDeLoteria/GestorParaVentaDeLoteria.Modelo/Sorteos.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations;
+
+namespace GestorParaVentaDeLoteria.Modelo
+{
+    public class Sorteos
+    {
+        public int Id { get; set; }
+
+        public Estado Estado { get; set; }
+
+        public DateTime? Fecha { get; set; }
+    }
+}
