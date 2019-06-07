ca1557342905150a38e20a829fad896211b38bbe
Pedro Pablo Lumbí Álvarez <ppablola-98@hotmail.com>
2019-06-06 23:31:20 -0600

    añadir SorteosRestriciones

---
+++ GestorParaVentaDeLoteria/GestorParaVentaDeLoteria.Modelo/SorteosRestriciones.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GestorParaVentaDeLoteria.Modelo
+{
+    public class SorteosRestriciones
+    {
+        public int Id { get; set; }
+        public int Numero { get; set; }
+        public decimal MontoMáximo { get; set; }
+        public int IdSorteos { get; set; }
+    }
+}
